0baf673fde820d7dcb4f3702b8540a54a170becc
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-06 12:10:40 +0100

    40 days from now

---
+++ DateTimeExercises/AddingDays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+
+            Console.WriteLine($"Today:\t\t\t{date.DayOfWeek}");
+            Console.WriteLine($"40 Days from now:\t{date.AddDays(40).DayOfWeek}");
+        }
+    }
+}
