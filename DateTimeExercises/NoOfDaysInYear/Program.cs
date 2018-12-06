caf88534c86fdf95bee4e7f2494cdb3b22bc92a8
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-06 11:06:17 +0100

    leap years

---
+++ DateTimeExercises/NoOfDaysInYear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoOfDaysInYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2001; i < 2010; i++)
+            {
+                var date = new DateTime(i, 12, 31);
+
+                if (DateTime.IsLeapYear(i))
+                {
+                    Console.WriteLine($"{date:MM/dd/yyyy}: day {date.DayOfYear} (Leap year)");
+                }
+                else
+                {
+                    Console.WriteLine($"{date:MM/dd/yyyy}: day {date.DayOfYear}");
+                }
+            }
+        }
+    }
+}
