c114e1ae7b9970b7a0ce5887ba2a6413f21286b8
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-06 11:13:03 +0100

    refactor

--- DateTimeExercises/NoOfDaysInYear/Program.cs
+++ DateTimeExercises/NoOfDaysInYear/Program.cs
@@ -6,22 +6,17 @@ using System.Threading.Tasks;
 
 namespace NoOfDaysInYear
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            for (int i = 2001; i < 2010; i++)
+            for (var i = 2001; i < 2030; i++)
             {
                 var date = new DateTime(i, 12, 31);
 
-                if (DateTime.IsLeapYear(i))
-                {
-                    Console.WriteLine($"{date:MM/dd/yyyy}: day {date.DayOfYear} (Leap year)");
-                }
-                else
-                {
-                    Console.WriteLine($"{date:MM/dd/yyyy}: day {date.DayOfYear}");
-                }
+                Console.WriteLine(DateTime.IsLeapYear(i)
+                    ? $"{date:dd/MM/yyyy}: Tag {date.DayOfYear} (Schaltjahr)"
+                    : $"{date:dd/MM/yyyy}: Tag {date.DayOfYear}");
             }
         }
     }
