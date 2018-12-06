e0307c49ab51d44f9e9082d45a0289de72063fef
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-06 11:24:00 +0100

    UTC

---
+++ DateTimeExercises/LocalUtc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalUtc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = DateTime.Now;
+
+            Console.WriteLine($"Local date and time:\t{dateTime}");
+            Console.WriteLine($"UTC date and time:\t{dateTime.ToUniversalTime()}");
+        }
+    }
+}
