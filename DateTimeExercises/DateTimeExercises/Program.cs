0f3d0ff9d0fe1c537881533a83a0e5c4ea565064
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-06 10:13:09 +0100

    dates

---
+++ DateTimeExercises/DateTimeExercises/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateOG = DateTime.Today.ToString();
+            var dateShort = dateOG.Substring(0, 10);
+            var date24Hlong = DateTime.Today.ToString("dd/MM/yyyy HH:mm");
+            var date24Hshort = DateTime.Today.ToString("M/d/yy") + DateTime.Today.ToString("g");
+
+          
+            Console.WriteLine($"Complete date:\t{DateTime.Now}");
+            Console.WriteLine($"Short date:\t{dateShort}");
+            Console.WriteLine($"24H Format:\t{date24Hlong}");
+            Console.WriteLine($"\t\t{date24Hshort}");
+        }
+    }
+}
